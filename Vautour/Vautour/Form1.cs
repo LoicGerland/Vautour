fdc7549c7f1ef4eb992a1867fb0f54401b9f3e3a
LoicGerland <loic.gerland@free.fr>
2015-03-19 09:49:42 +0100

    Add List for each IA and a List box for the hand of the player

--- Vautour/Vautour/Form1.cs
+++ Vautour/Vautour/Form1.cs
@@ -12,9 +12,35 @@ namespace Vautour
 {
     public partial class Form1 : Form
     {
+        List<int> CartesR; //Liste représentant la main du joueur et des IA
+        List<int> CartesJ;
+        List<int> CartesB;
+        List<int> CartesV;
+        List<int> CartesN;
+
         public Form1()
         {
             InitializeComponent();
+            CartesR = new List<int>();
+            CartesJ = new List<int>();
+            CartesB = new List<int>();
+            CartesV = new List<int>();
+            CartesN = new List<int>();
+
+            for(int i =1; i<=15; i++)
+            {
+                CartesR.Add(i);
+                CartesJ.Add(i);
+                CartesB.Add(i);
+                CartesV.Add(i);
+                CartesN.Add(i);
+            }
+
+            lb_main.DataSource = CartesR;
+            lb_main.Refresh();
         }
+
+        
+
     }
 }
