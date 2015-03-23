cc2da5ff16f9b2ffc408767ae83ec74d5af3ece3
LoicGerland <gerlandloic@gmail.com>
2015-03-23 12:21:23 +0100

    Copy of the project

---
+++ Vautour/Vautour/Carte.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vautour
+{
+    class Carte
+    {
+        private int value; //Valeur de la carte
+
+        private int indexImage; //Valeur de l'index dans le sabot pour récupérer l'image
+
+        private String couleur; //Couleur de la carte
+
+        public Carte(int v, int index, String c)
+        {
+            this.value = v;
+            this.indexImage = index;
+            this.couleur = c;
+        }
+
+        public int getValue()
+        {
+            return this.value;
+        }
+
+        public int getIndexImage()
+        {
+            return this.indexImage;
+        }
+
+        public String getCouleur()
+        {
+            return this.couleur;
+        }
+    }
+}
---
+++ Vautour/Vautour/IA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vautour
+{
+    class IA : Player
+    {
+        private int difficulty; //Difficulté de l'IA (1: facile, 2: Intermédiaire, 3: Difficile)
+
+        public IA(String n,List<Carte> m,int d) : base(n, m)
+        {
+            this.difficulty = d;
+        }
+    }
+}
